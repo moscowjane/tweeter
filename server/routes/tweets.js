23b01ae861160f19e8625a6b96f628c882bd6bb2
moscowjane <janedesbarats91@gmail.com>
2017-11-18 01:30:35 +0000

    tweets

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const userHelper    = require("../lib/util/user-helper")
+const userHelper    = require("../lib/util/user-helper");
 
 const express       = require('express');
 const tweetsRoutes  = express.Router();
@@ -8,39 +8,34 @@ const tweetsRoutes  = express.Router();
 module.exports = function(DataHelpers) {
 
   tweetsRoutes.get("/", function(req, res) {
-    DataHelpers.getTweets((err, tweets) => {
+    DataHelpers.getTweets((err, tweet) => {
       if (err) {
-        res.status(500).json({ error: err.message });
+        res.status(500).json({ error: "internal server error" });
       } else {
-        res.json(tweets);
+        res.json(tweet);
       }
     });
   });
-
   tweetsRoutes.post("/", function(req, res) {
     if (!req.body.text) {
       res.status(400).json({ error: 'invalid request: no data in POST body'});
       return;
     }
-
     const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
     const tweet = {
       user: user,
       content: {
         text: req.body.text
       },
-      created_at: Date.now()
+      createdAt: Date.now()
     };
-
     DataHelpers.saveTweet(tweet, (err) => {
       if (err) {
-        res.status(500).json({ error: err.message });
+        res.status(500).json({ error: "internal server error" });
       } else {
         res.status(201).send();
       }
     });
   });
-
   return tweetsRoutes;
-
-}
+};
